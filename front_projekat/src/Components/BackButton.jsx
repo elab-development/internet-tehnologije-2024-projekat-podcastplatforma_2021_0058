f1d92beef10615457ceb26ed866518a42d542f61
aknezevicc <ana.knezevic021@gmail.com>
2025-07-19 14:52:42 +0200

    Front projekat: izmenjen css za celokupan projekat, izmenjen gitignore fajl, dodate komponente za navigaciju i vracanje unazad na stranici

---
+++ front_projekat/src/Components/BackButton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./BackButton.module.css"; 
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); 
+  };
+
+  return (
+    <button className={styles.backButton} onClick={handleGoBack}>
+      Nazad
+    </button>
+  );
+};
+
+export default BackButton;
---
+++ front_projekat/src/Components/Navbar.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Navbar.css'; 
+
+const Navbar = ({ userRole }) => {
+  const [menuOpen, setMenuOpen] = useState(false); 
+  const navigate = useNavigate();
+
+ 
+  const handleLinkClick = () => {
+    setMenuOpen(false); 
+  };
+
+  const handleLogout = () => {
+    
+    window.sessionStorage.clear();
+
+
+    navigate('/');
+  };
+
+
+  const renderMenu = () => {
+    if (userRole === 'administrator') {
+      return (
+        <>
+          <li><Link to="/podkasti" onClick={handleLinkClick}>Podkasti</Link></li>
+          <li><Link to="/korisnici" onClick={handleLinkClick}>Korisnici</Link></li>
+          <li><Link to="/kategorije" onClick={handleLinkClick}>Kategorije</Link></li>
+        </>
+      );
+    }
+    if (userRole === 'kreator') {
+      return (
+        <>
+          <li><Link to="/podkasti" onClick={handleLinkClick}>Podkasti</Link></li>
+          <li><Link to="/kreirajPodkast" onClick={handleLinkClick}>Kreiraj Podkast</Link></li>
+          <li><Link to="/moji-podkasti" onClick={handleLinkClick}>Moji Podkasti</Link></li>
+          <li><Link to="/youtube" onClick={handleLinkClick}>Youtube</Link></li>
+        </>
+      );
+    }
+    if (userRole === 'gledalac') {
+      return (
+        <>
+          <li><Link to="/podkasti" onClick={handleLinkClick}>Podkasti</Link></li>
+          <li><Link to="/omiljeni" onClick={handleLinkClick}>Omiljeni Podkasti</Link></li>
+          <li><Link to="/youtube" onClick={handleLinkClick}>Youtube</Link></li>
+        </>
+      );
+    }
+  };
+
+  return (
+    <header className="navbar">
+      
+      <div className="burger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+ 
+      <div className="logo">
+        <h1>PODKAST</h1>
+      </div>
+
+   
+      <div className="logout">
+        <button onClick={handleLogout}>IZLOGUJ SE</button>
+      </div>
+
+      
+      <div className={`sidebar ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          {renderMenu()}
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
